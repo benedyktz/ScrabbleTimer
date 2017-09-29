9245077f4cc5270b710992b94fd1d7d8e99f15a1
Benedykt <ziolkowski.benedykt@gmail.com>
2017-09-29 23:16:02 +0200

    a click in stopped timer doesn't change running

--- app/src/main/java/com/benedyktgmail/ziolkowski/scrabbletimer/MainActivity.java
+++ app/src/main/java/com/benedyktgmail/ziolkowski/scrabbletimer/MainActivity.java
@@ -2,6 +2,7 @@ package com.benedyktgmail.ziolkowski.scrabbletimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -20,20 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         player1.timerValue = (TextView) findViewById(R.id.timerValue);
         player2.timerValue = (TextView) findViewById(R.id.timerValue2);
+        player1.running = true; // when player1 click his field, time start in player's 2 field
 
     }
 
     public void change(View view) {
-        
-        if(player1.running == true){
-            player1.stop();
-            player2.start();
+
+        if(player1.timerValue.getId() == view.getId()) {
+            if(player1.running == true){
+                player1.stop();
+                player2.start();
+            }
         }
-        else {
-            player2.stop();
-            player1.start();
+
+        if(player2.timerValue.getId() == view.getId()) {
+            if(player2.running == true){
+                player2.stop();
+                player1.start();
+            }
         }
+
     }
-
-
+    
 }
