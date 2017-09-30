152c9ac25d20e236027ba890471a9a141cbc0467
Benedykt <ziolkowski.benedykt@gmail.com>
2017-09-30 13:56:21 +0200

    4 players

--- app/src/main/java/com/benedyktgmail/ziolkowski/scrabbletimer/MainActivity.java
+++ app/src/main/java/com/benedyktgmail/ziolkowski/scrabbletimer/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     List<Field> players = new ArrayList<Field>();
     Field player1 = new Field();
     Field player2 = new Field();
+    Field player3 = new Field();
+    Field player4 = new Field();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +25,39 @@ public class MainActivity extends AppCompatActivity {
 
         player1.timerValue = (TextView) findViewById(R.id.timerValue);
         player2.timerValue = (TextView) findViewById(R.id.timerValue2);
+        player3.timerValue = (TextView) findViewById(R.id.timerValue3);
+        player4.timerValue = (TextView) findViewById(R.id.timerValue4);
         players.add(player1);
         players.add(player2);
+        players.add(player3);
+        players.add(player4);
         for (Field player: players) {
             player.running = true;
         }
     }
 
-
+    int i = 0;
 
     public void change(View view) {
 
-        if(players.get(0).timerValue.getId() == view.getId()) {
-            if(players.get(0).running == true){
-                players.get(0).stop();
-                players.get(1).start();
+        if (i == 3) {
+            if(players.get(i).timerValue.getId() == view.getId()) {
+                if(players.get(i).running == true){
+                    players.get(i).stop();
+                    players.get(0).start();
+                }
+            }
+            i=0;
+        }
+
+        if(players.get(i).timerValue.getId() == view.getId()) {
+            if(players.get(i).running == true){
+                players.get(i).stop();
+                players.get(i+1).start();
+                i++;
             }
         }
 
-        if(players.get(1).timerValue.getId() == view.getId()) {
-            if(players.get(1).running == true){
-                players.get(1).stop();
-                players.get(0).start();
-            }
-        }
 
     }
 
